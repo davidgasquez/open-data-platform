f912d89ea8808440b0e4e8ddd00c69e3a2ea19f0
David Gasquez <davidgasquez@gmail.com>
2023-01-18 17:41:59 +0000

    feat: ⚡️

--- osds/__init__.py
+++ osds/__init__.py
@@ -1,17 +1,21 @@
 from dagster import Definitions
 
-from dagster._utils import file_relative_path
+from dagster_dbt import dbt_cli_resource
+from dagster_dbt import load_assets_from_dbt_project
 
-from osds.assets import dagster_hits
+DBT_PROJECT_DIR = "dbt/"
 
-from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
-
-DBT_DIR = file_relative_path(__file__, "../modeling")
-
-dbt_resource = (
-    dbt_cli_resource.configured({"project_dir": DBT_DIR, "profiles_dir": DBT_DIR}),
+dbt_resource = dbt_cli_resource.configured(
+    {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROJECT_DIR}
 )
 
-dbt_assets = load_assets_from_dbt_project(DBT_DIR, DBT_DIR)
+dbt_assets = load_assets_from_dbt_project(DBT_PROJECT_DIR, DBT_PROJECT_DIR)
 
-defs = Definitions(assets=[dagster_hits, *dbt_assets], resources={"dbt": dbt_resource})
+resources = {
+    "dbt": dbt_resource,
+}
+
+defs = Definitions(
+    assets=[*dbt_assets],
+    resources=resources,
+)
--- osds/assets.py
+++
@@ -1,9 +0,0 @@
-from dagster import asset
-
-import pandas as pd
-
-
-@asset
-def hits():
-    # TODO: Download from Clickhouse S3
-    return pd.DataFrame([{"hits": 1}])
